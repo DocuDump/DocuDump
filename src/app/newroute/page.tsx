9cfc7c1991ec9dc038f554843e8d48add88d7e16
Daniel Blittschau <d@dblitt.com>
2024-01-29 00:45:01 -0500

    Config updates

--- src/app/newroute/page.tsx
+++
@@ -1,8 +0,0 @@
-export default function Page() {
-    let thing = [
-        "long  ksdfdsfj fksdj ",
-        "fjdskjf ksdjf kdsj ",
-        "fdsjfkjsdkfjsdk",
-    ];
-    return <h1>Hello, Next.js!</h1>;
-}
