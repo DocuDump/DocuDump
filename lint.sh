6de758d31a918d724f12913ecb3c3dd816b2e36a
Daniel Blittschau <d@dblitt.com>
2024-02-21 18:55:43 -0500

    Bring up GHA CI (linting) (#13)

---
+++ lint.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+set -uo pipefail
+
+EXIT_CODE=0
+
+error() {
+    EXIT_CODE=1
+}
+
+cleanup() {
+    if [ $EXIT_CODE -ne 0 ]; then
+        echo
+        echo "Exiting with errors."
+    fi
+
+    exit $EXIT_CODE
+}
+
+trap "error" ERR
+trap "cleanup" EXIT
+
+npm run lint
+
+npm run prettier
