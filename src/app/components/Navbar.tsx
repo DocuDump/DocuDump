403faea1ef34aba223a3862b4e27d667a07bed5f
collinb424 <collinb424@gmail.com>
2024-02-26 16:51:09 -0500

    fix hydration and add more responsiveness

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -5,29 +5,29 @@ import { MdAccountCircle } from "react-icons/md";
 function Navbar() {
     return (
         <nav className="inset-x-0 top-0 flex h-20 items-center justify-between bg-blue-500 text-white shadow-sm">
-            <div className="pl-8">
+            <div className="px-4 md:px-8">
                 {/* TODO - 20: Add a logo */}
 
                 <Link
                     href="/home"
-                    className="text-xl font-semibold hover:text-gray-300"
+                    className="text-base font-semibold hover:text-gray-300 md:text-xl"
                 >
                     DocuDump
                 </Link>
             </div>
-            <div className="flex items-center">
+            <div className="flex items-center justify-between gap-2 md:gap-8">
                 <Link
                     href="#"
-                    className="rounded bg-white px-4 py-2 font-bold text-blue-500 hover:bg-blue-700 hover:text-gray-200"
+                    className="rounded bg-white px-2 py-1.5 font-bold text-blue-500 hover:bg-blue-700 hover:text-gray-200 md:px-4 md:py-2"
                 >
                     Log in
                 </Link>
-                <Link href="#" className="pl-4 hover:text-gray-200 md:pl-10">
+                <Link href="#" className=" hover:text-gray-200 ">
                     <IoSettingsSharp size="36" />
                 </Link>
                 <Link
                     href="/dashboard"
-                    className="px-4 hover:text-gray-200 md:px-10"
+                    className="pr-3 hover:text-gray-200 md:pr-6 "
                 >
                     <MdAccountCircle size="36" />
                 </Link>
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,11 +18,11 @@ export default function RootLayout({
 }>) {
     return (
         <html lang="en">
-            <body className={`min-h-screen ${inter.className} flex-grow`}>
+            <body className={`min-h-screen ${inter.className} flex flex-col`}>
                 <Navbar />
-                {children}
+                <div className="flex-grow">{children}</div>
+                <Footer />
             </body>
-            <Footer />
         </html>
     );
 }
