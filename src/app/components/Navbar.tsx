b77d7346def17a543673b1f5f3cf2248111b81f0
collinb424 <92958582+collinb424@users.noreply.github.com>
2024-02-28 17:24:51 -0500

    Create header and footer (#23)
    Create responsive header and footer with icons
    <img width="948" alt="image"
    src="https://github.com/DocuDump/DocuDump/assets/92958582/a5016b4f-4d58-4877-adee-4e1d23692c5e">
    
    Footer:
    <img width="960" alt="image"
    src="https://github.com/DocuDump/DocuDump/assets/92958582/34b9c739-8f18-4b39-9b98-034a4c1e9281">

---
+++ src/app/components/Navbar.tsx
@@ -0,0 +1,40 @@
+import Link from "next/link";
+import { IoSettingsSharp } from "react-icons/io5";
+import { MdAccountCircle } from "react-icons/md";
+import styles from "./Navbar.module.css";
+
+function Navbar() {
+    return (
+        <nav className="inset-x-0 top-0 flex h-16 items-center justify-between bg-blue-500 text-white shadow-sm md:h-20">
+            <div className="px-4 md:px-8">
+                {/* TODO - 20: Add a logo */}
+
+                <Link
+                    href="/home"
+                    className="text-lg font-semibold hover:text-gray-300 md:text-2xl"
+                >
+                    DocuDump
+                </Link>
+            </div>
+            <div className="flex items-center justify-between gap-3 md:gap-8">
+                <Link
+                    href="#"
+                    className="rounded bg-white px-1.5 py-1 text-sm font-bold text-blue-500 hover:bg-blue-700 hover:text-gray-200 md:px-4 md:py-2 md:text-lg"
+                >
+                    Log in
+                </Link>
+                <Link href="#" className="hover:text-gray-200">
+                    <IoSettingsSharp className={styles.iconBase} />
+                </Link>
+                <Link
+                    href="/dashboard"
+                    className="pr-3 hover:text-gray-200 md:pr-6"
+                >
+                    <MdAccountCircle className={styles.iconBase} />
+                </Link>
+            </div>
+        </nav>
+    );
+}
+
+export default Navbar;
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,8 +18,10 @@ export default function RootLayout({
 }>) {
     return (
         <html lang="en">
-            <body className={`min-h-screen ${inter.className}`}>
-                {children}
+            <body className={`min-h-screen ${inter.className} flex flex-col`}>
+                <Navbar />
+                <div className="flex-grow">{children}</div>
+                <Footer />
             </body>
         </html>
     );
