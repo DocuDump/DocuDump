3bd7db5426fc4562693b7008a851486d06ff9e3f
kpconnors <kconnor3@nd.edu>
2024-02-28 13:15:16 -0500

    Redid dashboard table. Made logic simpler and UI cleaner

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,81 +1,104 @@
 "use client";
 import React, { useState } from "react";
-import TableHeadCell from "../components/Table/headCell";
-import TableBodyCell from "../components/Table/bodyCell";
 
 const Dashboard = () => {
     //hard-coded pastes
     const pastes = [
         {
-            name: "First Paste",
+            title: "First Paste",
             type: "PDF",
             pub_date: "02/17/2025",
             exp_date: "02/22/2025",
         },
         {
-            name: "Second Paste",
+            title: "Second Paste",
             type: "Text",
             pub_date: "02/18/2025",
             exp_date: "02/23/2025",
         },
         {
-            name: "Third Paste",
+            title: "Third Paste",
             type: "Link",
             pub_date: "02/19/2025",
             exp_date: "02/24/2025",
         },
         {
-            name: "Fourth Paste",
+            title: "Fourth Paste",
             type: "IMG",
             pub_date: "02/20/2025",
             exp_date: "02/25/2025",
         },
     ];
 
-    const handlePasteClick = (pasteName: String) => {
+    const handlePasteClick = (pasteName: string) => {
         //Todo #10: Create view page for specifc paste. User can navigate there through this click
         alert(`Clicked on ${pasteName}`);
     };
 
     return (
         <div>
-            <table style={{ width: "100%", borderCollapse: "collapse" }}>
-                <thead>
+            <table className="w-full text-left text-sm text-gray-500 rtl:text-right dark:text-gray-400">
+                <thead className="bg-gray-50 text-xs uppercase text-gray-700 dark:bg-gray-700 dark:text-gray-400">
                     <tr>
-                        <TableHeadCell width="40%" align="left">
-                            Name
-                        </TableHeadCell>
-                        <TableHeadCell width="20%" align="center">
+                        <th
+                            scope="col"
+                            className="px-6 py-3"
+                            style={{ width: "40%" }}
+                        >
+                            Title
+                        </th>
+                        <th
+                            scope="col"
+                            className="px-6 py-3"
+                            style={{ width: "10%" }}
+                        >
                             Type
-                        </TableHeadCell>
-                        <TableHeadCell width="20%" align="center">
+                        </th>
+                        <th
+                            scope="col"
+                            className="px-6 py-3"
+                            style={{ width: "25%" }}
+                        >
                             Publication Date
-                        </TableHeadCell>
-                        <TableHeadCell width="20%" align="center">
+                        </th>
+                        <th
+                            scope="col"
+                            className="px-6 py-3"
+                            style={{ width: "25%" }}
+                        >
                             Expiration Date
-                        </TableHeadCell>
+                        </th>
                     </tr>
                 </thead>
                 <tbody>
                     {pastes.map((paste, index) => (
-                        <tr key={index}>
-                            <TableBodyCell width="40%" align="left">
+                        <tr
+                            key={index}
+                            className="border-b bg-white hover:bg-gray-50 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-600"
+                        >
+                            <td
+                                className="whitespace-nowrap px-6 py-4 font-medium text-gray-900 dark:text-white"
+                                style={{ width: "40%" }}
+                            >
                                 <a
                                     href="#"
-                                    onClick={() => handlePasteClick(paste.name)}
+                                    onClick={() =>
+                                        handlePasteClick(paste.title)
+                                    }
+                                    className="hover:underline"
                                 >
-                                    {paste.name}
+                                    {paste.title}
                                 </a>
-                            </TableBodyCell>
-                            <TableBodyCell width="20%" align="center">
+                            </td>
+                            <td className="px-6 py-4" style={{ width: "10%" }}>
                                 {paste.type}
-                            </TableBodyCell>
-                            <TableBodyCell width="20%" align="center">
+                            </td>
+                            <td className="px-6 py-4" style={{ width: "25%" }}>
                                 {paste.pub_date}
-                            </TableBodyCell>
-                            <TableBodyCell width="20%" align="center">
+                            </td>
+                            <td className="px-6 py-4" style={{ width: "25%" }}>
                                 {paste.exp_date}
-                            </TableBodyCell>
+                            </td>
                         </tr>
                     ))}
                 </tbody>
