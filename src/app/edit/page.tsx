d8a2d982730633861b6b1dbcc47f6c28dd551998
kpconnors <kconnor3@nd.edu>
2024-02-28 23:56:10 -0500

    Converted edit page to a modal

--- src/app/edit/page.tsx
+++ src/app/edit/page.tsx
@@ -1,14 +1,25 @@
 "use client";
 import React, { useState, useEffect } from "react";
+import Modal from "react-modal";
 
 interface PasteData {
     [key: string]: string;
 }
 
-const Edit = () => {
+interface EditProps {
+    paste: {
+        title: string;
+        type: string;
+        pub_date: string;
+        exp_date: string;
+    };
+}
+
+const Edit: React.FC<EditProps> = ({ paste }) => {
+    const [modalIsOpen, setModalIsOpen] = useState(false);
     const [pasteData, setPasteData] = useState<PasteData>({});
 
-    //change labels when we decide on all editable atttributes of paste
+    // change lables when we determine what needs to be editable
     const labels = {
         title: "Title",
         type: "Type",
@@ -16,17 +27,12 @@ const Edit = () => {
         exp_date: "Experation Date",
     };
 
-    //here will be the logic to get the specific paste to view/edit, for now it is hard coded data
-    const paste = {
-        title: "Example Paste",
-        type: "PDF",
-        pub_date: "02/17/2025",
-        exp_date: "02/22/2025",
-    };
-
     useEffect(() => {
-        setPasteData(paste);
-    }, []);
+        if (paste) {
+            setPasteData(paste);
+            openModal();
+        }
+    }, [paste]);
 
     const handleInputChange = (label: string, value: string) => {
         setPasteData({
@@ -35,37 +41,83 @@ const Edit = () => {
         });
     };
 
+    const openModal = () => {
+        setModalIsOpen(true);
+    };
+
+    const closeModal = () => {
+        setModalIsOpen(false);
+    };
+
+    const handleSubmit = (e: React.FormEvent) => {
+        e.preventDefault();
+        // Add logic for api call to edit paste
+        closeModal();
+    };
+
+    const modalStyles: Modal.Styles = {
+        overlay: {
+            backgroundColor: "rgba(0, 0, 0, 0.5)",
+        },
+        content: {
+            borderRadius: "20px",
+            width: "50%",
+            top: "50%",
+            left: "50%",
+            right: "auto",
+            bottom: "auto",
+            marginRight: "-50%",
+            transform: "translate(-50%, -50%)",
+        },
+    };
+
     return (
-        <form className="mx-auto" style={{ width: "80%" }}>
-            <div className="mt-5 grid grid-cols-2 gap-4">
-                {Object.entries(labels).map(([key, labelText]) => (
-                    <div className="mb-5" key={key}>
-                        <label
-                            htmlFor={key}
-                            className="mb-1 ml-2 flex gap-2 text-sm font-medium text-gray-700"
-                        >
-                            {labelText}
-                        </label>
-                        <input
-                            type="text"
-                            id={key}
-                            value={pasteData[key] || ""}
-                            onChange={(e) =>
-                                handleInputChange(key, e.target.value)
-                            }
-                            className="w-full rounded-lg border border-gray-300 p-2.5 text-sm transition-colors duration-200 focus:border-blue-500 focus:outline-none focus:ring-blue-500"
-                            placeholder={`Enter ${labelText}`}
-                            required
-                        />
+        <div>
+            <Modal
+                isOpen={modalIsOpen}
+                onRequestClose={closeModal}
+                contentLabel="Edit Modal"
+                style={modalStyles}
+            >
+                <form
+                    className="mx-auto"
+                    style={{ width: "80%" }}
+                    onSubmit={handleSubmit}
+                >
+                    <div className="mt-5 grid grid-cols-2 gap-4">
+                        {Object.entries(labels).map(([key, labelText]) => (
+                            <div className="mb-5" key={key}>
+                                <label
+                                    htmlFor={key}
+                                    className="mb-1 ml-2 flex gap-2 text-sm font-medium text-gray-700"
+                                >
+                                    {labelText}
+                                </label>
+                                <input
+                                    type="text"
+                                    id={key}
+                                    value={pasteData[key] || ""}
+                                    onChange={(e) =>
+                                        handleInputChange(key, e.target.value)
+                                    }
+                                    className="w-full rounded-lg border border-gray-300 p-2.5 text-sm transition-colors duration-200 focus:border-blue-500 focus:outline-none focus:ring-blue-500"
+                                    placeholder={`Enter ${labelText}`}
+                                    required
+                                />
+                            </div>
+                        ))}
                     </div>
-                ))}
-            </div>
-            <div className="mt-5 flex justify-center">
-                <button className="mx-auto h-10 rounded-lg bg-blue-500 px-5 py-2.5 text-sm font-medium text-white hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50">
-                    Submit
-                </button>
-            </div>
-        </form>
+                    <div className="mt-5 flex justify-center">
+                        <button
+                            className="mx-auto h-10 rounded-lg bg-blue-500 px-5 py-2.5 text-sm font-medium text-white hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50"
+                            type="submit"
+                        >
+                            Submit
+                        </button>
+                    </div>
+                </form>
+            </Modal>
+        </div>
     );
 };
 
