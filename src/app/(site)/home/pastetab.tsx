3e7b1606f3618c2edc4a4a88986c858d58883915
collinb424 <collinb424@gmail.com>
2024-03-03 12:45:06 -0500

    add monaco editor for line numbers and syntax highlighting

--- src/app/(site)/home/pastetab.tsx
+++ src/app/(site)/home/pastetab.tsx
@@ -1,38 +1,62 @@
 "use client";
 
 import React, { useState } from "react";
+import Editor from "@monaco-editor/react";
 import Settings from "./settings";
+import { LanguageKey } from "@/app/types";
+import { languageOptions } from "@/app/constants";
 
 function PasteTab() {
-    const [pasteText, setPasteText] = useState("");
-    const [syntaxHighlight, setSyntaxHighlight] = useState("None");
+    const [currentText, setCurrentText] = useState(""); // Holds the current text
+    const [language, setLanguage] = useState<LanguageKey>("plaintext");
 
-    // TODO: #14 - Add syntax highlighting and line numbers in textarea
+    const handleLanguageChange = (
+        event: React.ChangeEvent<HTMLSelectElement>,
+    ) => {
+        const newLanguage = event.target.value as LanguageKey;
+        setLanguage(newLanguage);
+        setCurrentText("");
+    };
 
     return (
         <div className="space-y-4">
             <div className="text-right">
                 <select
-                    value={syntaxHighlight}
-                    onChange={(e) => setSyntaxHighlight(e.target.value)}
+                    value={language}
+                    onChange={handleLanguageChange}
                     className="rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-sm outline-none"
                 >
-                    <option value="Python">Python</option>
-                    <option value="Java">Java</option>
+                    {Object.entries(languageOptions).map(([key, display]) => (
+                        <option key={key} value={key}>
+                            {display}
+                        </option>
+                    ))}
                 </select>
             </div>
 
             <div>
-                <textarea
-                    id="message"
-                    placeholder="Paste your content here..."
-                    rows={12}
-                    maxLength={1000}
-                    onChange={(e) => {
-                        setPasteText(e.target.value);
+                <Editor
+                    height="50vh"
+                    defaultLanguage="plaintext"
+                    language={language}
+                    value={currentText} // Use the current text
+                    onChange={(value) => {
+                        // Check if the incoming value is not undefined before updating the state
+                        if (value !== undefined) {
+                            setCurrentText(value);
+                        }
                     }}
-                    className="w-full rounded-lg border border-gray-300 bg-gray-50 bg-transparent p-2.5 pl-2 transition-colors duration-200 focus:border-blue-500 focus:outline-none focus:ring-blue-500"
-                ></textarea>
+                    theme="vs-light"
+                    options={{
+                        lineNumbers: "on",
+                        scrollBeyondLastLine: false,
+                        readOnly: false,
+                        minimap: {
+                            enabled: false,
+                        },
+                    }}
+                    className="w-full rounded-sm border bg-gray-50 transition-colors duration-200"
+                />
             </div>
             <Settings />
 
