04792c1934d02b991a3beebff69a2dc971df5e9a
collinb424 <92958582+collinb424@users.noreply.github.com>
2024-03-04 11:43:32 -0500

    Restructure into (site) directory (#37)
    - Reorganized Header, Footer, and Dashboard into app/(site) directory to
    make application more extensible.
    - Note that this is a redo of the earlier PR to account for large
    directory changes.

--- src/app/(site)/dashboard/page.tsx
+++ src/app/(site)/dashboard/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 import React, { useState } from "react";
-import EditModal from "@/app/components/EditModal";
+import EditModal from "@/app/(site)/components/EditModal";
 import Modal from "react-modal";
 
 const Dashboard = () => {
---
+++ src/app/(site)/layout.tsx
@@ -0,0 +1,18 @@
+import Navbar from "@/app/(site)/components/Navbar";
+import Footer from "@/app/(site)/components/Footer";
+
+export default function SiteLayout({
+    children,
+}: {
+    children: React.ReactNode;
+}) {
+    return (
+        <section>
+            <div className={`flex min-h-screen flex-col`}>
+                <Navbar />
+                <main className="mb-20 flex-grow">{children}</main>
+                <Footer />
+            </div>
+        </section>
+    );
+}
