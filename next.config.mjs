43aae8b46bebd461e8c8e2b974f6c9ff253c7333
kpconnors <kconnor3@nd.edu>
2024-04-23 21:04:59 -0400

    Issue #51 fix

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: "standalone",
+    // redirects '/' to '/home'
+    async redirects() {
+        return [
+            {
+                source: "/",
+                destination: "/home",
+                permanent: true,
+            },
+        ];
+    },
 };
 
 export default nextConfig;
