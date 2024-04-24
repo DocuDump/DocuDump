46b460c23f60fbf20056c8cd5d76eca2f429f247
kpconnors <kconnor3@nd.edu>
2024-04-23 23:13:41 -0400

    Changed redirect logic

--- next.config.mjs
+++ next.config.mjs
@@ -1,16 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: "standalone",
-    // redirects '/' to '/home'
-    async redirects() {
-        return [
-            {
-                source: "/",
-                destination: "/home",
-                permanent: true,
-            },
-        ];
-    },
 };
 
 export default nextConfig;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,23 +1,5 @@
-import { getAllUserNames, getAllUsers } from "@/db";
+import { redirect } from "next/navigation";
 
 export default async function Home() {
-    return (
-        <main>
-            <p className="font-bold">hello world</p>
-            <p className="font-mono">getAllUserNames():</p>
-            <ol>
-                {getAllUserNames().map((name, index) => (
-                    <li key={index}>{name}</li>
-                ))}
-            </ol>
-            <p className="font-mono">getAllUsers():</p>
-            <ol>
-                {getAllUsers().map((user, index) => (
-                    <li key={index}>
-                        {user.name}: {user.email ?? "email is null"}
-                    </li>
-                ))}
-            </ol>
-        </main>
-    );
+    redirect("/home");
 }
